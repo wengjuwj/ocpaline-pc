d9afc8a77daae880977748a1a9f64a3eeb9a308c
wengjuwj <18226137228@163.com>
2021-08-24 17:06:29 +0800

    update

--- .postcssrc.js
+++ .postcssrc.js
@@ -6,7 +6,8 @@ module.exports = {
     },
     'postcss-pxtorem': {
       rootValue: 37.5, //vant-UI的官方根字体大小是37.5
-      propList: ['*']
+      propList: ['*'],
+      exclude: /pc/i
     }
   }
 }
--- src/views/pc/SearchList.vue
+++ src/views/pc/SearchList.vue
@@ -1,18 +1,45 @@
 <template>
   <div class="pc-full-contain">
-     <div class="pc-contain">
-        <div class="breadcrumb-box">
-            <el-breadcrumb separator-class="el-icon-arrow-right">
-              <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
-              <el-breadcrumb-item>活动管理</el-breadcrumb-item>
-              <el-breadcrumb-item>活动列表</el-breadcrumb-item>
-              <el-breadcrumb-item>活动详情</el-breadcrumb-item>
-            </el-breadcrumb>
+    <div>
+        <div class="pc-contain">
+            <div class="breadcrumb-box">
+                <el-breadcrumb separator-class="el-icon-arrow-right">
+                  <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+                  <el-breadcrumb-item>关键词搜索</el-breadcrumb-item>
+                </el-breadcrumb>
+            </div>
         </div>
-     </div>
+    </div>
+     
      <!-- 搜索结果 -->
      <div class="search-content">
-
+       <div class="pc-contain">
+         <div class="total-show">
+           共有<span>12</span>门包含“<span>甲沟炎</span>”的直播课程
+         </div>
+          <div class="card-item" v-for="(item,index) in cardList" :key="index">
+            <el-card :body-style="{ padding: '0px' }">
+                <div class="card-selfdefine" @click="cardEmit"> 
+                  <div class="card-left" @click.stop="imgEmit">
+                    <img alt="" :src="require('@/assets/card1.png')">
+                    <div class="mark" v-if="c==1">
+                        <div>今日直播</div>
+                    </div>
+                  </div>
+                  <div class="card-right">
+                      <div class="right-title" @click.stop="titleEmit">针对治好强直性脊柱炎的关键点剖析</div>
+                      <div class="tags-content">
+                        <span @click.stop="tagsEmit">专家</span>
+                        <span>宣讲</span>
+                        <span>手术</span>
+                      </div>
+                      <!-- <div class="info">主讲人：<span>上海市第五人民医院 廖朱权主任</span></div> -->
+                      <div class="info">时间：<span>2021-07-21-12:30</span></div>
+                  </div>
+                </div>
+            </el-card>
+            </div>
+       </div>
      </div>
   </div>
 </template>
@@ -25,7 +52,7 @@ export default {
   },
   data(){
     return{
-      
+      cardList:[1,2,3,4]
     }
   },
   methods:{
@@ -36,9 +63,118 @@ export default {
 
 <style lang="less" scoped>
   .breadcrumb-box{
-      padding: 15px 0;
+      padding: 25px 0 15px 0;
+      
+      /deep/ .el-breadcrumb{
+        font-size: 14px;
+      }
   }
   .search-content{
-    
+    width: 100%;
+    box-shadow: -7px -2px 7px #e6e6e6;
+    background: #F6F6F6;
+    .total-show{
+      padding: 30px 0;
+      font-size: 14px;
+      color: #999;
+      span{
+        color: #1B97EE;
+      }
+    }
   }
+  .card-item{
+        cursor: pointer;
+        margin-bottom: 20px;
+        .img-wrap{
+          width: 100%;
+          height: 175px;
+          position: relative;
+          img{
+            width: 100%;
+            height: 100%;
+          }
+          .mark{
+            position: absolute;
+            top: 20px;
+            right: 20px;
+            width: 30%;
+            height: 30px;
+            border-radius: 4px;
+            background: rgba(0, 0, 0, 0.6);
+            color: #fff;
+            justify-content: center;
+            display: flex;
+            align-items: center;
+          }
+        }
+        .content-wrap{
+          padding: 14px;
+          .title{
+             font-size: 16px;
+          }
+          .bottom{
+            display: flex;
+            justify-content: space-between;
+            font-size: 14px;
+            color: #999;
+            margin-top: 10px;
+            span{
+              margin-right: 6px;
+            }
+          }
+        }
+        // 左右排列的卡片样式
+        .card-selfdefine{
+          display: flex;
+          // padding: 15px;
+          align-items: center;
+          .card-left{
+            width: 38%;
+            height: 245px;
+            position: relative;
+             .mark{
+              position: absolute;
+              top: 0;
+              left: 0;
+              width: 40%;
+              height: 30px;
+              border-radius: 15px 0;
+              background: linear-gradient(#1678D3,#1B97EE);
+              color: #fff;
+              justify-content: center;
+              display: flex;
+              align-items: center;
+            }
+           img{
+              max-width: 100%;
+              height: 100%;
+              border-radius: 15px 0 0 15px;
+           }
+          }
+          .card-right{
+            margin-left: 10px;
+            .right-title{
+              font-size: 16px;
+              margin-bottom: 30px;
+            }
+            .tags-content{
+              span{
+                padding:0 7px 0 7px;
+                background: #d5ecfd;
+                color: #1B97EE;
+                border-radius: 2px;
+                margin-right: 10px;
+                &:last-child{
+                  margin-right: 0;
+                }
+              }
+            }
+            .info{
+              font-size: 14x;
+              color: #999;
+              margin-top: 10px;
+            }
+          }
+        }
+      }
 </style>
